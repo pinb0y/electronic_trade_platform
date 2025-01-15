87c33158475d0b56c0527468f1b91cfdeb0b7cf2
Pinchuk Sergey <Pinboyer@gmail.com>
2025-01-16 01:24:18 +0300

    add CRUD for electric_trade

--- config/settings.py
+++ config/settings.py
@@ -21,9 +21,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
 
     'electronic_trade',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
